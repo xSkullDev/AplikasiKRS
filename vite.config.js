c62376171f158a948093787fe721863ba30648be
xSkullDev <x.skull.dev@gmail.com>
2026-07-05 18:31:30 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  base: process.env.VITE_BASE_PATH || "/AplikasiKRS",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
